aa3d320d78a9e2df9c08b4d00e90028aa82d957a
Sandeep Deb <deb.sandeep@gmail.com>
2016-09-26 08:40:48 +0000

    Drawing a rhombus

---
+++ 02-List/rhombus.py
@@ -0,0 +1,25 @@
+import sys
+
+sideLength = int(raw_input( 'Please enter side length of rhonus : ' ))
+lines = []
+bottomLines = []
+
+for i in range( sideLength ):
+    line = []
+    arr = [ ' ' ]*sideLength
+
+    arr[sideLength-i-1] = '*'
+    line += arr
+    arr.reverse()
+    line += arr[1:]
+    
+    lines.append( line )
+    bottomLines.insert( 0, line[:] )
+
+lines += bottomLines[1:]
+
+print
+for line in lines:
+    for char in line:
+        sys.stdout.write( char )
+    print
