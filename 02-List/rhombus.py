eb32c82cecc6528d50a115569976ef20f4deba4a
argho94 <dcpune94@gmail.com>
2016-10-01 09:42:05 +0530

    Update rhombus.py

--- 02-List/rhombus.py
+++ 02-List/rhombus.py
@@ -1,19 +1,19 @@
 import sys
 
-sideLength = int(raw_input( 'Please enter side length of rhombus : ' ))
-lines = []
-bottomLines = []
+sideLength = int(raw_input( 'Please enter side length of rhombus : ' )) #number of rows of rhombus triangle
+lines = [] #for upper triangle 
+bottomLines = [] #for lower triangle
 
 for i in range( sideLength ):
     line = []
-    arr = [ ' ' ]*sideLength
+    arr = [ ' ' ]*sideLength #arr will have sidelength number of spaces
 
-    arr[sideLength-i-1] = '*'
-    line += arr
-    arr.reverse()
-    line += arr[1:] 
+    arr[sideLength-i-1] = '*' #2n-1 stars for inner and outer respectively
+    line += arr #print line of stars since rhombus is symmetric about all axes
+    arr.reverse() #lower triangle
+    line += arr[1:] #exclude character 1 
     
-    lines.append( line )
+    lines.append( line ) #drawing line of stars
     bottomLines.insert( 0, line[:] )
 
 lines += bottomLines[1:]
