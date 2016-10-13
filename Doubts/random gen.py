85b7626956523de12ef4c76cd99ae4300111107f
Sandeep Deb <deb.sandeep@gmail.com>
2016-10-13 12:01:13 +0530

    Random gen answer

--- Doubts/random gen.py
+++ Doubts/random gen.py
@@ -8,9 +8,7 @@ for i in range(10):
     # random class assigned against the 'random' variable. In the above line
     # you are calling the random() method on the random class instance.
 
-    print (x) # how can i print integers?
-    # [Sandy] - Not sure if I understood the question. 
-
-# Got it. 
-
-#My doubt was something like, the above code will print random float values. How can i print random integer values?
+    print ( int(x*100) ) 
+    # how can i print integers?
+    # [Sandy] If you need three digit integers, multiply by 1000. In general
+    # multiply by 10**n if you need n digit randon values.
